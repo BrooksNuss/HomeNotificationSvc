ffca96d376da44930188534f0572b30cc2a002c3
Brooks Nuss <bnussjr@gmail.com>
2023-04-13 23:15:03 -0400

    Added lambda layer

--- lambda/index.ts
+++ lambda/index.ts
@@ -110,7 +110,7 @@ async function handleWebsocketEvent(event: APIGatewayProxyWebsocketEventV2): Pro
 					connectionId: { S: event.requestContext.connectionId }
 				},
 				ExpressionAttributeValues: {
-					":subs": { "SS": [body.subscriptionType] }
+					':subs': { 'SS': [body.subscriptionType] }
 				}
 			};
 			try {
@@ -159,110 +159,3 @@ async function sendNotificationToConnection(conn: HomeWSConnection, body: HomeWS
 		headers,
 	};
 }
-	// try {
-	// 	const id = event.pathParameters?.id || '';
-	// 	const fields = event.body ? JSON.parse(event.body) : null;
-	// 	const feeder = id ? await getFeeder(id) : null;
-
-	// 	switch (event.resource as FeederApiResources) {
-	// 	case '/activate/{id}':
-	// 		if (!feeder) {
-	// 			throw `Could not find feeder with id [${id}]`;
-	// 		}
-	// 		console.log('Received activate message for feeder {%s}', id);
-	// 		if (feeder.status === 'OFFLINE') {
-	// 			throw `Feeder [${id}] is offline`;
-	// 		} else {
-	// 			try {
-	// 				await postSqsMessage({id, type: 'activate'});
-	// 				body = 'Success';
-	// 			} catch(e) {
-	// 				console.error(e);
-	// 				body = 'Error posting SQS message: ' + e;
-	// 			}
-	// 		}
-	// 		break;
-	// 	case '/list-info':
-	// 		console.log('Received get list message');
-	// 		body = await getFeederList();
-	// 		break;
-	// 	case '/skip/{id}':
-	// 		if (!feeder) {
-	// 			throw `Could not find feeder with id [${id}]`;
-	// 		}
-	// 		console.log('Received skip message for feeder {%s}', id);
-	// 		if (feeder.status === 'OFFLINE') {
-	// 			throw `Feeder [${id}] is offline`;
-	// 		} else {
-	// 			try {
-	// 				await postSqsMessage({id, type: 'skip'});
-	// 				body = 'Success';
-	// 			} catch(e) {
-	// 				console.error(e);
-	// 				body = 'Error posting SQS message: ' + e;
-	// 			}
-	// 		}
-	// 		break;
-	// 	case '/toggle-enabled/{id}':
-	// 		if (!feeder) {
-	// 			throw `Could not find feeder with id [${id}]`;
-	// 		}
-	// 		console.log('Received toggle message for feeder {%s}', id);
-	// 		try {
-	// 			await postSqsMessage({id, type: 'toggle-enabled'});
-	// 			body = 'Success';
-	// 		} catch(e) {
-	// 			console.error(e);
-	// 			body = 'Error posting SQS message: ' + e;
-	// 		}
-	// 		break;
-	// 	case '/update/{id}':
-	// 		if (!feeder) {
-	// 			throw `Could not find feeder with id [${id}]`;
-	// 		}
-	// 		console.log('Received update message for feeder {%s}', id);
-	// 		try {
-	// 			await postSqsMessage({id, type: 'update', fields });
-	// 			body = 'Success';
-	// 		} catch(e) {
-	// 			console.error(e);
-	// 			body = 'Error posting SQS message: ' + e;
-	// 		}
-	// 	}
-	// } catch (err: any) {
-	// 	console.error(err);
-	// 	statusCode = 400;
-	// 	body = err.message;
-	// } finally {
-	// 	body = JSON.stringify(body);
-	// }
-
-
-// async function getFeederList(): Promise<FeederInfo[]> {
-// 	console.log('Fetching feeder list from DynamoDB');
-// 	const params: ScanInput = {
-// 		TableName: 'feeders'
-// 	};
-// 	const queryResult = dynamo.scan(params).promise();
-// 	return (await queryResult).Items as FeederInfo[];
-// }
-
-// async function getFeeder(id: string): Promise<FeederInfo> {
-// 	console.log('Fetching feeder by id: ' + id);
-// 	// key type in docs is different from what the sdk expects. type should be GetItemInput
-// 	const params = {
-// 		TableName: 'feeders',
-// 		Key: {id}
-// 	};
-// 	const queryResult = await dynamo.get(params).promise();
-// 	return queryResult.Item as FeederInfo;
-// }
-
-// async function postSqsMessage(body: FeederSqsMessage) {
-// 	console.log('Posting message to SQS');
-// 	const params: SendMessageRequest = {
-// 		QueueUrl: feederQueueUrl || '',
-// 		MessageBody: JSON.stringify(body)
-// 	};
-// 	return sqs.sendMessage(params).promise();
-// }
---
+++ lambda/lambdaLayer/README.md
@@ -0,0 +1,3 @@
+To prepare the Lambda layer for upload, npm install using the package.json in the nodejs folder.
+
+Once the modules are downloaded, zip the entire nodejs folder into nodejs.zip This zip file can be uploaded as a Lambda layer.
